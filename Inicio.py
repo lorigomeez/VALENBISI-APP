9d549f4d20dd7d0a3bdc0e3e7e6097dae029ce8c
lorigomeez <94955739+lorigomeez@users.noreply.github.com>
2023-06-26 22:19:48 +0200

    Add files via upload

---
+++ Inicio.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 22 21:44:23 2023
+
+@author: Usuario
+"""
+
+import streamlit as st
+
+
+import plotly.graph_objects as go
+import pandas as pd
+from tabulate import tabulate
+
+ruta_archivo = "C:/Users/Usuario/Desktop/UNI/EDM/trabajo/primavera/valenbisi_procesado.csv"
+
+# Cargar el archivo CSV en un DataFrame
+df = pd.read_csv(ruta_archivo)
+
+
+page_title = 'VALENBISI Datos históricos'
+layout = 'wide'
+
+st.set_page_config(page_title = page_title, layout = layout)
+
+st.title(page_title)
+st.sidebar.success("Selecciona una página")
+
+Hora = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13,14,15,16,17,18,19,20,21,22,23]
+Dia = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+Mes = [1,2,3,4,5,6,7,8,9,10,11,12]
+Anyo = [2022,2023]
+graficos = ['Bicicletas disponibles', 'Espacios libres disponibles', 'Estaciones cerradas']
+
+st.write('Selecciona un día, mes, año y hora, y haz click en Guardar datos.')
+with st.form('entry_form', clear_on_submit = False):
+    col1, col2, col3, col4 = st.columns(4)
+    col1.selectbox("Selecciona día:" , Dia, key="day")
+    col2.selectbox("Selecciona mes:" , Mes, key="month")
+    col3.selectbox("Selecciona año:" , Anyo, key="year")
+    col4.selectbox("Selecciona hora:" , Hora, key="hour")
+    col1.selectbox("¿Qué quieres ver? ", graficos, key='figure')
+    
+    "---"
+    submitted = st.form_submit_button('Guardar datos')
+    if submitted:
+        selected_day = st.session_state.day
+        selected_month = st.session_state.month
+        selected_year = st.session_state.year
+        selected_hour = st.session_state.hour
+        selected_figure = st.session_state.figure
+
+        st.success('Datos guardados')
+
+        st.header('Resultado')
+        
+        if selected_figure == 'Bicicletas disponibles':
+            df_filtrado = df[(df['Mes'] == selected_month) & (df['Dia'] == selected_day) & (df['Hora'] == selected_hour) & (df['Año'] == selected_year)]
+            df_top_10 = df_filtrado.sort_values(by='avg_av', ascending=False).head(10)
+            import plotly.express as px
+            fig = px.bar(df_top_10, x='name', y='avg_av',
+                         color='avg_av',
+                         labels={'name': 'Estación', 'avg_av': 'Media de bicis disponibles'},
+                         title=f'Las 10 estaciones con más bicis disponibles para los datos introducidos')
+            fig.update_layout(margin = dict(l=0, r=0, t =30, b = 5))
+            st.plotly_chart(fig, use_container_width=True)
+            
+            
+
+        if selected_figure == 'Espacios libres disponibles':
+            df_filtrado = df[(df['Mes'] == selected_month) & (df['Dia'] == selected_day) & (df['Hora'] == selected_hour) & (df['Año'] == selected_year)]
+            df_top_10 = df_filtrado.sort_values(by='avg_free', ascending=False).head(10)
+            import plotly.express as px
+            fig = px.bar(df_top_10, x='name', y='avg_free',
+                         color='avg_free',
+                         labels={'name': 'Estación', 'avg_free': 'Media de espacios libres'},
+                         title=f'Las 10 estaciones con más espacios libres disponibles para los datos introducidos')
+            fig.update_layout(margin = dict(l=0, r=0, t =30, b = 5))
+            st.plotly_chart(fig, use_container_width=True)
+
+        if selected_figure == 'Estaciones cerradas':
+            st.write('Estaciones cerradas')
+            datos = df[(df['open'] == 'F') & (df['Mes'] == selected_month) & (df['Dia'] == selected_day) & (df['Hora'] == selected_hour) & (df['Año'] == selected_year)]
+            datos = datos[['name', 'address', 'number_']]
+            datos = datos.rename(columns={'name': 'Estación', 'address': 'Dirección', 'number_': 'Número'}).style.hide_index()
+
+            styles = [
+    {"selector": "table", "props": [("border-collapse", "collapse")]},
+    {"selector": "th, td", "props": [("border", "1px solid #ccc"), ("padding", "8px")]},
+    {"selector": "th", "props": [("background-color", "#f2f2f2"), ("color", "#333")]}
+]
+
+            style_string = ""
+            for style in styles:
+                selector = style["selector"]
+                props = "; ".join([f"{prop[0]}: {prop[1]}" for prop in style["props"]])
+                style_string += f"{selector} {{ {props} }} "
+            
+            styled_table = f"<style>{style_string}</style>"
+            st.markdown(styled_table, unsafe_allow_html=True)
+
+            st.table(datos)
+
+
+
+
+
+
+
---
+++ Predicción.py
@@ -0,0 +1,102 @@
+import streamlit as st
+import pandas as pd
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LinearRegression
+
+
+ruta_archivo = "C:/Users/Usuario/Desktop/UNI/EDM/trabajo/primavera/valenbisi_procesado_coordenadas.csv"
+
+# Cargar el archivo CSV en un DataFrame
+data = pd.read_csv(ruta_archivo)
+
+#CREAR MODELO
+# Seleccionar las variables relevantes para la predicción
+variables = ['Dia', 'Mes', 'Año', 'Hora','name']
+target = ['avg_av','avg_free', 'avg_total']
+
+# Realizar codificación one-hot para la variable "name"
+name_encoder = OneHotEncoder(sparse=False)
+column_transformer = make_column_transformer((name_encoder, ['name']), remainder='passthrough')
+data_encoded = column_transformer.fit_transform(data[variables])
+
+# Obtener los nombres de las características después de la codificación one-hot
+name_categories = list(column_transformer.named_transformers_['onehotencoder'].categories_[0])
+feature_names = name_categories + variables[1:]
+
+# Crear el conjunto de datos de entrenamiento
+X = data_encoded
+y = data[target]
+
+# Crear el modelo de regresión lineal
+model = LinearRegression()
+
+# Entrenar el modelo
+model.fit(X, y)
+
+
+st.title("Predicción de disponibilidad")
+
+
+
+Hora = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13,14,15,16,17,18,19,20,21,22,23]
+Dia = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+Mes = [1,2,3,4,5,6,7,8,9,10,11,12]
+Nombre=list(data['name'].unique())
+Anyo = [2023,2024]
+graficos = ['Bicicletas disponibles', 'Espacios libres']
+
+with st.form('entry_form', clear_on_submit = False):
+    col1, col2, col3 = st.columns(3)
+    col1.selectbox("Selecciona día:" , Dia, key="day")
+    col2.selectbox("Selecciona mes:" , Mes, key="month")
+    col3.selectbox("Selecciona año:" , Anyo, key="year")
+    col1.selectbox("Selecciona hora:" , Hora, key="hour")
+    col2.selectbox("¿Qué quieres predecir?", graficos, key='figure')
+    col3.selectbox('Selecciona estación: ', Nombre, key = 'name')
+    submitted = st.form_submit_button(label='Guardar datos')
+    if submitted:
+        selected_day = st.session_state.day
+        selected_month = st.session_state.month
+        selected_year = st.session_state.year
+        selected_hour = st.session_state.hour
+        selected_name = st.session_state.name
+        selected_figure = st.session_state.figure
+        
+
+        st.success('Datos guardados')
+
+        
+        if selected_figure == 'Bicicletas disponibles':
+            nuevos_datos = pd.DataFrame([[selected_day, selected_month, selected_year, selected_hour, selected_name]], columns=variables)
+            nuevos_datos_encoded = column_transformer.transform(nuevos_datos)
+            
+            # Realizar la predicción
+            prediccion = model.predict(nuevos_datos_encoded)
+            
+            #avg_av
+            st.write("Media de bicicletas disponibles: ", round(prediccion[0][0]))
+
+            
+            
+
+        if selected_figure == 'Espacios libres':
+            nuevos_datos = pd.DataFrame([[selected_day, selected_month, selected_year, selected_hour, selected_name]], columns=variables)
+            nuevos_datos_encoded = column_transformer.transform(nuevos_datos)
+            
+            # Realizar la predicción
+            prediccion = model.predict(nuevos_datos_encoded)
+            
+            # Mostrar la predicción
+            st.subheader("Resultados de la predicción:")
+            st.write(prediccion)
+            
+            #avg_av
+            st.write("Media de espacios libres: ",prediccion[0][1])
+
+            
+            
+
+
+
+
