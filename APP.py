96e177a874d2464a9dea551256a6f540f9391788
lorigomeez <94955739+lorigomeez@users.noreply.github.com>
2023-06-27 13:47:35 +0200

    Update APP.py

--- APP.py
+++ APP.py
@@ -164,7 +164,7 @@ if submit_button:
         tu_longitud = longitud   
             
         # DataFrame con las estaciones
-        df_estaciones = pd.read_csv("https://github.com/lorigomeez/VALENBISI-APP/solo_coordenadas.csv")  # Reemplaza "archivo.csv" con el nombre de tu archivo CSV
+        df_estaciones = pd.read_csv("solo_coordenadas.csv")  # Reemplaza "archivo.csv" con el nombre de tu archivo CSV
         df_estaciones["distancia"] = df_estaciones.apply(
             lambda row: geodesic((tu_latitud, tu_longitud), (row["geo_point_2d"].split(',')[0], row["geo_point_2d"].split(',')[1])).kilometers,
             axis=1
